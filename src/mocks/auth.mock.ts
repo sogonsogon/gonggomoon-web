b9f252205a2efbb2b94b0dcecb655caf301c69ec
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2026-03-04 17:15:59 +0900

    feat: User, Recruitment 타입 변경에 따른 user, recruitment 목업 데이터 변경

--- src/mocks/auth.mock.ts
+++ src/mocks/auth.mock.ts
@@ -1,9 +1,8 @@
 import { User } from '@/features/auth/types';
 
 export const mockUser: User = {
-  id: 'u_1001',
-  email: 'sooyeon.kim@example.com',
-  name: '김수연',
+  email: 'sogonsogon@example.com',
+  name: '소곤이',
   profileImageUrl: 'https://picsum.photos/seed/user1001/200/200',
   roles: ['MEMBER'],
 };
