4ba22781e5d514f19f7158c26fd5fcc11e2f2140
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2026-03-09 11:46:15 +0900

    feat: Company 도메인 타입 수정
    - API 명세 변경 반영

--- src/features/company/types.ts
+++ src/features/company/types.ts
@@ -1,3 +1,5 @@
+import { IndustryType } from '@/features/industry/types';
+
 export type CompanyType =
   | 'LARGE_ENTERPRISE'
   | 'MID_SIZED_ENTERPRISE'
@@ -6,11 +8,18 @@ export type CompanyType =
 
 export type Company = {
   companyId: number;
+  industryType: IndustryType;
   companyName: string;
   companyType: CompanyType;
   industryId?: number;
+  description?: string; // 기업 소개
+  revenue?: number; // 매출액
   websiteUrl?: string;
   foundedYear?: number;
   address?: string;
   employeeCount?: number;
+  createdBy?: number;
+  updatedBy?: number;
+  createdAt?: string;
+  updatedAt?: string;
 };
