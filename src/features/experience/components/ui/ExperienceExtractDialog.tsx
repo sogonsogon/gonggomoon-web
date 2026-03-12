1eedcfa67b8061b9fba12a1cc4705c7388d50751
seyun0714 <seun0714@naver.com>
2026-03-12 09:40:42 +0900

    fix: File 타입 수정에 따른 필드명 수정

--- src/features/experience/components/ui/ExperienceExtractDialog.tsx
+++ src/features/experience/components/ui/ExperienceExtractDialog.tsx
@@ -96,11 +96,11 @@ export default function ExperienceExtractDialog({ files }: ExperienceExtractDial
             {/* 파일 리스트 */}
             <div className="flex flex-col gap-2 py-2">
               {files.map((file) => {
-                const isSelected = selectedFileIds.has(file.fileId);
+                const isSelected = selectedFileIds.has(file.fileAssetId);
                 const isDisabled = !isSelected && selectedFileIds.size >= MAX_FILE_SELECTIONS;
                 return (
                   <ExperienceFileItem
-                    key={file.fileId}
+                    key={file.fileAssetId}
                     file={file}
                     isSelected={isSelected}
                     isDisabled={isDisabled}
