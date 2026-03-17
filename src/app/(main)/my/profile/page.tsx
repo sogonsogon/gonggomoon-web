4bf35f35ca2d1d89e67fcfbec759c928d03c684a
seyun0714 <seun0714@naver.com>
2026-03-17 11:09:56 +0900

    style: profile 페이지 스타일 통일

--- src/app/(main)/my/profile/page.tsx
+++ src/app/(main)/my/profile/page.tsx
@@ -5,7 +5,7 @@ export default async function ProfilePage() {
   return (
     <div className="flex flex-col w-full">
       {/* 메인 영역*/}
-      <div className="flex flex-1 flex-col gap-8">
+      <div className="flex flex-1 flex-col gap-6">
         {/* 페이지 타이틀 */}
         <Title title={'프로필'} description={'계정 정보를 확인하고 관리할 수 있습니다'} />
 
