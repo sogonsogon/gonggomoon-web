c544db4fd6bbfb8353f46b6d6fbd9e46a2d17191
seyun0714 <seun0714@naver.com>
2026-03-17 11:30:42 +0900

    fix: mypage mx 통일

--- src/app/(main)/my/layout.tsx
+++ src/app/(main)/my/layout.tsx
@@ -6,7 +6,7 @@ export default function MyLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex flex-col flex-1 w-full gap-4 px-4 pt-6 pb-10 md:flex-row md:min-h-180 md:px-0 md:pt-10 md:pb-20 md:gap-12">
+    <div className="flex flex-col flex-1 w-full gap-4 px-4 pt-6 pb-10 md:flex-row md:min-h-180 md:pt-10 md:pb-20 md:gap-12">
       <MyNav />
       {children}
     </div>
