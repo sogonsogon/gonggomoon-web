12e6a70bb9eed5fb11d6acc1270df25e2669dfd3
seyun0714 <seun0714@naver.com>
2026-03-21 15:22:40 +0900

    chore: sentry 도입

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+import { withSentryConfig } from '@sentry/nextjs';
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
@@ -19,4 +20,18 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+  org: 'none-ake',
+
+  project: 'gonggomoon-service',
+
+  silent: !process.env.CI,
+
+  widenClientFileUpload: true,
+
+  webpack: {
+    treeshake: {
+      removeDebugLogging: true,
+    },
+  },
+});
